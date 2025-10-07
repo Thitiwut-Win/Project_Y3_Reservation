eef2536bfbce6fb437f74e7b8ff63a4cbd670f75
Thitiwut-Win <bmwin9825@gmail.com>
2025-10-07 23:58:08 +0700

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+const apiUrl = process.env.API_URL;
 
 const nextConfig: NextConfig = {
   /* config options here */
