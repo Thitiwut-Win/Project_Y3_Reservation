79af343542cf2af653e2d0f1e44043aef7709790
Thitiwut-Win <bmwin9825@gmail.com>
2025-10-08 16:35:51 +0700

    Update page.tsx

--- frontend/src/app/tickets/page.tsx
+++ frontend/src/app/tickets/page.tsx
@@ -1,10 +1,7 @@
 import PaymentPageClient from "@/components/PaymentPageClient";
-import { Suspense } from "react";
 
 export default function PaymentPage() {
 	return (
-		<Suspense fallback={<p>Loading payment...</p>}>
-			<PaymentPageClient />
-		</Suspense>
+		<PaymentPageClient />
 	);
 }
