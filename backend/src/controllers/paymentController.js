561bc572b2e6e8ba220fd5275c69717ec961b2b6
Thitiwut-Win <bmwin9825@gmail.com>
2025-12-03 23:03:11 +0700

    Update payment to scb api

--- backend/src/controllers/paymentController.js
+++ backend/src/controllers/paymentController.js
@@ -1,10 +1,9 @@
 import Payment from "../models/Payment.js";
 // import Event from "../models/Event.js";
 import qrcode from "qrcode";
-import generatePayload from "promptpay-qr";
 import { v4 as uuidv4 } from 'uuid';
 import crypto from "crypto";
-import {nanoid} from "nanoid"
+import {customAlphabet} from "nanoid"
 
 async function fetchAccessToken (uuid) {
   const response = await fetch("https://api-sandbox.partners.scb/partners/sandbox/v1/oauth/token", {
@@ -13,6 +12,9 @@ async function fetchAccessToken (uuid) {
 			"Content-Type": "application/json",
 			"requestUId": uuid,
 			"resourceOwnerId": process.env.SCB_API_KEY,
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0 Safari/537.36",
+      "Accept": "application/json, text/plain, */*",
+      "Accept-Language": "en-US,en;q=0.9",
 		},
 		body: JSON.stringify({
 			applicationKey: process.env.SCB_API_KEY,
@@ -20,28 +22,31 @@ async function fetchAccessToken (uuid) {
 		}),
 	})
   const data = await response.json();
-  return {
-    status: response.status,
-    data
-  };
+  return data;
 }
 
 async function fetchQR (token, userId, eventId, amount, uuid) {
 
   const date = new Date().toISOString().slice(0, 10).replace(/-/g, "");
   const random1 = Math.random().toString(36).substring(2, 8).toUpperCase();
-  const random2 = nanoid(15);
+  const generateRef3 = customAlphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 15);
+  const random2 = generateRef3();
+  
   const ref1 = `TXN${date}${random1}`;
-  const ref2 = crypto.createHash("sha256").update(`${userId}:${eventId}:${process.env.HASH_SECRET}`).digest("hex").slice(0, 20);
+  const ref2 = crypto.createHash("sha256").update(`${userId}:${eventId}:${process.env.HASH_SECRET}`).digest("hex").slice(0, 20).toUpperCase();
   const ref3 = `${process.env.REF_PREFIX}${random2}`;
 
-  const response = await ("https://api-sandbox.partners.scb/partners/sandbox/v1/payment/qrcode/create", {
+  console.log(ref1, ref2, ref3)
+  const response = await fetch("https://api-sandbox.partners.scb/partners/sandbox/v1/payment/qrcode/create", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
 			"resourceOwnerId": process.env.SCB_API_KEY,
 			"requestUId": uuid,
-			"authorization": token
+			"authorization": `Bearer ${token}`,
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0 Safari/537.36",
+      "Accept": "application/json, text/plain, */*",
+      "Accept-Language": "en-US,en;q=0.9",
 		},
 		body: JSON.stringify({
 			qrType: "PP",
@@ -54,10 +59,7 @@ async function fetchQR (token, userId, eventId, amount, uuid) {
 		})
 	})
   const data = await response.json();
-  return {
-    status: response.status,
-    data
-  };
+  return data;
 }
 
 export const createPayment = async (req, res) => {
@@ -72,26 +74,22 @@ export const createPayment = async (req, res) => {
     const uuid = uuidv4();
     const tokenResponse = await fetchAccessToken(uuid);
     if (!tokenResponse) return res.status(400).json({ message: "Error request access token" });
-    console.log(tokenResponse.status, tokenResponse.data)
+    console.log(tokenResponse)
 
 		const token = tokenResponse.data.accessToken;
     const QRResponse = await fetchQR(token, userId, eventId, amount, uuid);
     if (!QRResponse) return res.status(400).json({ message: "Error requesting QR" });
-    console.log(QRResponse.status.code, QRResponse.status.description)
-
-    const promptpayId = process.env.PROMPTPAY_ID;
-
-    const qrString = generatePayload(promptpayId, { amount });
+    console.log(QRResponse)
 
     const payment = await Payment.create({
       userId: req.userId,
       eventId,
       amount,
       seats,
-      qrString,
+      qrString: QRResponse.data.qrRawData,
     });
 
-    const qrImage = await qrcode.toDataURL(qrString);
+    const qrImage = await qrcode.toDataURL(QRResponse.data.qrRawData);
 
     res.json({
       success: true,
