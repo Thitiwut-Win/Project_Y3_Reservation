8882561eb5367e72fcd0b7bee40cf3848e96f4a8
Thitiwut-Win <bmwin9825@gmail.com>
2025-12-04 20:47:44 +0700

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -144,7 +144,6 @@ export default function HomePage() {
 						{events.slice(0, 3).map((event) => (
 							<motion.li
 								key={event._id}
-								variants={fadeUp}
 								className="bg-white dark:bg-gray-800 p-6 rounded-xl shadow-md hover:shadow-xl transition-all
                   hover:-translate-y-1 duration-300"
 							>
